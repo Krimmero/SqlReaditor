a9b49f80bf25f8492d9ab1f33418c8e2969adabc
KimStationær <krimmero@hotmail.dk>
2018-01-02 06:11:33 +0100

    DataAdapter and Reader

--- SqlReaditor/MainWindow.xaml.cs
+++ SqlReaditor/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace SqlReaditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -34,133 +34,51 @@ namespace SqlReaditor
 
         private void BtnList_OnClick(object sender, RoutedEventArgs e)
         {
-            listBox.Items.Add(GetSelectedCellValue());
+            string connectionString =
+                ConnectionStringHolder.ConString;
+            string selected = listBox.SelectedItem.ToString();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                string query = "SELECT * FROM " + selected;
+                SqlCommand cmd = new SqlCommand(query, connection);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable(selected);
+                sda.Fill(dt);
+                dataGrid.ItemsSource = dt.DefaultView;
+                
+            }
         }
         private void btnLoadDB_Click(object sender, RoutedEventArgs e)
         {
             lblUser.Content = ConnectionStringHolder.Username;
-           
-
-
-            //StackPanel stack = new StackPanel();
-            //stack.HorizontalAlignment = HorizontalAlignment.Left;
             
-            //stack.Height = 400;
-            //stack.Width = 400;
-            //stack.Background = Brushes.White;
-            //mainGrid.Children.Add(stack);
 
-            //Button btn = new Button();
-            //btn.Height = 100;
-            //btn.Width = 100;
-            //btn.Content = "Button";
-            //btn.Foreground = Brushes.Blue;
-            //btn.Background = Brushes.Red;
-            //stack.Children.Add(btn);
-
-            //Label label = new Label();
-            //label.Height = 40;
-            //label.Width = 70;
-            //label.Foreground = Brushes.Black;
-            //label.Content = "This is a label";
-            //stack.Children.Add(label);
-             
             string connectionString =
-               ConnectionStringHolder.ConString;
-            SqlConnection connection = new SqlConnection(connectionString);
+                ConnectionStringHolder.ConString;
 
+            List<string> tableList = new List<string>();
 
-
-
-            try
+            using (SqlConnection connection = new SqlConnection(connectionString))
             {
-                if (connection.State == ConnectionState.Closed)
+                connection.Open();
+                string query = "SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE='BASE TABLE'";
+                SqlCommand cmd = new SqlCommand(query, connection);
+                var reader = cmd.ExecuteReader();
+
+                while (reader.Read())
                 {
-                    connection.Open();
+                    tableList.Add(reader.GetString(0));
                 }
-                else
+
+                foreach (var table in tableList)
                 {
-                    MessageBox.Show("Database not found");
+                    listBox.Items.Add(table);
                 }
 
-                
-                var tables = connection.GetSchema("Tables");
-
-                dataGrid.SetBinding(ItemsControl.ItemsSourceProperty, new System.Windows.Data.Binding { Source = tables });
-
-
-                var cellInfo = dataGrid.SelectedCells[0];
-                
-                var content = cellInfo.Column.GetCellContent(cellInfo.Item);
-                SelectedItem.FrameworkElement = content;
-
-
-
-                //foreach (DataRow table in tables.Rows)
-                //{
-                //    listBox.Items.Add(table.ToString());
-                //}
-
-
-                //string query = "select table_schema from information_schema.tables where table_type='base table'";
-                //SqlCommand sqlCmd = new SqlCommand(query, connection);
-                //sqlCmd.CommandType = CommandType.Text;
-
-
-
-
-
-
-
-
-
-
-
-
-                //String query =
-                //    "SELECT ConnectionString FROM Users WHERE UserName=@Username";
-                //SqlCommand sqlCmd = new SqlCommand(query, connection);
-                //sqlCmd.CommandType = CommandType.Text;
-                //sqlCmd.Parameters.AddWithValue("@Username", .Text);
-                //sqlCmd.Parameters.AddWithValue("@ConnectionString", encryptedConnectionString);
-                //sqlCmd.Parameters.AddWithValue("@Password", hash);
-                //sqlCmd.Parameters.AddWithValue("@Salt", salt);
-
-
-                //if (sqlCmd.ExecuteNonQuery() != 0)
-                //{
-                //    LoginScreen login = new LoginScreen();
-                //    login.Show();
-                //    this.Close();
-                //}
-                //else
-                //{
-                //    MessageBox.Show("Failed creating new user!");
-                //}
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-            finally
-            {
                 connection.Close();
-            }
+
+              }
         }
-
-        public string GetSelectedCellValue()
-        {
-            DataGridCellInfo cellInfo = dataGrid.SelectedCells[0];
-            if (cellInfo == null) return null;
-
-            DataGridBoundColumn column = cellInfo.Column as DataGridBoundColumn;
-            if (column == null) return null;
-
-            FrameworkElement element = new FrameworkElement() { DataContext = cellInfo.Item };
-            BindingOperations.SetBinding(element, TagProperty, column.Binding);
-
-            return element.Tag.ToString();
-        }
-
     }
 }
